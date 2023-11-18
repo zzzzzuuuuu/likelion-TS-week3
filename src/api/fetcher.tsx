1da08041d5e7d2e3148edbe45c49bc44a002326b
zzzzzuuuuu <agu0830@naver.com>
2023-11-19 00:12:54 +0900

    feat: 공통된 fetch함수 분리

--- src/api/fetcher.tsx
+++ src/api/fetcher.tsx
@@ -1,4 +1,20 @@
+import { RegisterFormValues } from '../pages/Register';
+
 export const fetcher = async (url: string) => {
   const res = await fetch(url);
   return res.json();
+};
+
+export const postFetcher = async (
+  url: string,
+  { arg }: { arg: RegisterFormValues },
+) => {
+  const res = await fetch(`${process.env.REACT_APP_BASE_URL}${url}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(arg), // 서버한테 전달
+  });
+  return res;
 };
